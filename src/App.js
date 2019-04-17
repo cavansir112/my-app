7f14864b0ce1f93f250360d72eaeea1938d8295b
Muradov Cavanshir Imanqulu  <Mura_lw91@itstep.edu.az>
2019-04-17 11:44:31 +0400

    my-app2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import User from "./components/user"
+import './components/test.scss'
 import './App.css';
+import { Button } from './components/Button';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     <User/>
-     <User/>
-
-     <User/>
-
+        <User/>
+        <Button/>
       </div>
     );
   }
--- src/components/user.js
+++ src/components/user.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
+import { Button } from './components/Button';
 
 class user extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <div className="nav">
           <ul>
-            <a>jasdfnjd</a>
-            <a>jasdfnjd</a>
-            <a>jasdfnjd</a>
-            <a>jasdfnjd</a>
-            <a>jasdfnjd</a>
-          </ul>       
+            <li>jasdfnjd</li>
+            <li>jasdfnjd</li>
+            <li>jasdfnjd</li>
+            <li>jasdfnjd</li>
+            <li>jasdfnjd</li>
+          </ul>    
         </div>
       </div>
     )
   }
 }
 export default user;
+
+
+class Button extends Component{
+  render() {
+    return (
+      <buttton className="button"></buttton>
+    )
+  }
+  
+}
+
+export default Button;
