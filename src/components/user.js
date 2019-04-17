83916d446ab84f87a3724ab30fafb2204d5c98aa
Muradov Cavanshir Imanqulu  <Mura_lw91@itstep.edu.az>
2019-04-17 11:00:47 +0400

    my-app

---
+++ src/components/user.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class user extends Component {
+  render() {
+    return (
+      <div>
+        <div className="nav">
+          <ul>
+            <a>jasdfnjd</a>
+            <a>jasdfnjd</a>
+            <a>jasdfnjd</a>
+            <a>jasdfnjd</a>
+            <a>jasdfnjd</a>
+          </ul>       
+        </div>
+      </div>
+    )
+  }
+}
+export default user;
